1bfcbb01345f002e1182a8f91032a6eacc413008
Okhrymenko <Ohrimenko15@gmail.com>
2026-03-15 13:42:53 +0200

    fix some issues in templates and static files

--- todo_project/settings.py
+++ todo_project/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = "todo_project.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [BASE_DIR / "templates"],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
@@ -131,3 +131,4 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 LOGIN_URL = "login"
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL = "/registration/login/"
